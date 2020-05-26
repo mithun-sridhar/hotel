7f8bfd6d265022aa6815eff8498e375813eb6ba1
mithun-sridhar <65328130+mithun-sridhar@users.noreply.github.com>
2020-05-26 12:07:31 +0530

    Add files via upload

---
+++ hotel.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas
+import argparse
+import connect
+parser = argparse.ArgumentParser()
+parser.add_argument("--page_num_max",help="Enter the number of pagess to parse",type=int)
+parser.add_argument("--dbname",help="Enter the name of db",type=int)
+args = parser.parse_args()
+oyo_url = "https://www.oyorooms.com/hotels-in-bangalore/?page="
+page_num_MAX = args.page_num_max
+scraped_info_list=[]
+connect.connect(args.dbname)
+for i in range(page_num_MAX):
+    url=oyo_url+str(page_num_MAX)
+    print("GET Request for:"+url)
+    req = requests.get(url)
+    content = req.content
+    soup = BeautifulSoup(content,"html.parser")
+    all_hotels = soup.find_all("div",{"class":"hotelCardListing"})
+
+    for hotel in all_hotels:
+        hotel_dict={}
+        hotel_dict["name"]=hotel.find("h3",{"class":"listingHotelDescription__hotelName"}).text
+        hotel_dict["address"]=hotel.find("span",{"itemprop":"streetAddress"}).text
+        hotel_dict["price"]=hotel.find("span",{"class":"listingPrice__finalPrice"}).text
+        try:
+            hotel_dict["rating"]=hotel.find("span",{"class":"hotelRating__ratingSummary"}).text
+        except AttributeError:
+            hotel_dict["rating"]=None   
+        parent_amenities_element=hotel.find("div",{"class","amenityWrapper"})
+        amenities_list=[]
+        for amenity in parent_amenities_element.find_all("div",{"class","amenityWrapper__ammenity"}):
+            amenities_list.append(amenity.find("span",{"class":"d-body-sm"})).text.strip()
+            hotel_dict["amenities"]='.'.join(amenities_list[:-1])
+            scraped_info_list.append(hotel_dict)
+            connect.insert_info_table(args.dbname, tuple(hotel_dict.values()))
+        print(hotel_dict["name"], hotel_dict["address"],hotel_dict["price"],hotel_dict["rating"], hotel_dict["amenities_list"])
+dataFrame = pandas.DataFrame(scraped_info_list)
+dataFrame.to_csv("oyo.csv")
+connect.get_hotel_info(args.dbname)
